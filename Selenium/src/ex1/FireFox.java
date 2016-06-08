71088624e4418d3056edf116a04f9c433a297307
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-08 18:56:58 -0400

    added search functionality

--- Selenium/src/ex1/FireFox.java
+++ Selenium/src/ex1/FireFox.java
@@ -1,5 +1,6 @@
 package ex1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,6 +10,11 @@ public class FireFox {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.w3schools.com");
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.className("fa-search")).click();
+		driver.findElement(By.id("gsc-i-id1")).sendKeys("javascript");
+		driver.findElement(By.className("gsc-search-button")).click();
+		driver.get(driver.findElement(By.linkText("JavaScript Introduction")).getAttribute("href"));
 	}
 	
 
