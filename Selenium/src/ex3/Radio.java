7a5ccd97658405d7603de9074106d8d4a44deb60
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-09 17:59:13 -0400

    Lecture 27 - radio

---
+++ Selenium/src/ex3/Radio.java
@@ -0,0 +1,40 @@
+package ex3;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class Radio {
+
+
+
+
+
+	public static void main (String[] args) throws InterruptedException{
+		WebDriver driver = new FirefoxDriver();
+
+		driver.get("http://www.echoecho.com/htmlforms10.htm");
+		driver.manage().window().maximize();
+		List<WebElement> list = driver.findElements(By.name("group1"));
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getAttribute("checked") == null){ // null or true
+				list.get(i).click();
+				Thread.sleep(3000);
+			}
+		}
+		
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).getAttribute("checked") == null){ // null or true
+				System.out.println(list.get(i).getAttribute("value"));
+				Thread.sleep(3000);
+			}
+		}
+
+		driver.close();
+	}
+}
+
+
+
