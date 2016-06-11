4f3019ee60c5027c3ccf1aec910634fcca41b8fd
dvngnt <dgantt2@students.towson.edu>
2016-06-11 15:00:35 -0400

    Lecture 31 - Dragging in Selenium

--- Selenium/src/ex3/Draggable.java
+++ Selenium/src/ex3/Draggable.java
@@ -3,23 +3,31 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 public class Draggable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver = new FirefoxDriver();
-		//driver.get("https://jqueryui.com/resources/demos/draggable/default.html");//first way: add frame src to get url
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\dgantt2\\Downloads\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://jqueryui.com/draggable/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		//driver.switchTo().frame(0); //second way use switchTo().frame and index
-		driver.switchTo().frame(driver.findElement(By.xpath(".//*[@id='content']/iframe"))); // third way buy xpath and switch to
+		driver.switchTo().frame(0); //second way use switchTo().frame and index
+		
+		
 		try {
-			System.out.println(driver.findElement(By.id("draggable")).getText()); 
-			driver.switchTo().defaultContent(); //switch back to main page
-			System.out.println(driver.findElement(By.className("entry-title")).getText());
-		} catch (org.openqa.selenium.NoSuchElementException e) {
+			WebElement drag = driver.findElement(By.id("draggable")); 
+			System.out.println(drag.getText());
+			Actions action = new Actions(driver);
+			action.dragAndDropBy(drag, 50,50).build().perform();;
+		} 
+		
+		
+		catch (org.openqa.selenium.NoSuchElementException e) {
 			System.out.println("it isn't working");
 		}
 
