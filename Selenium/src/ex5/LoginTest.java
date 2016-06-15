620bf0833c917c263d4001a33d7c552692ca66fc
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-15 17:21:38 -0400

    Lecture 59 - xml batch files

---
+++ Selenium/src/ex5/LoginTest.java
@@ -0,0 +1,23 @@
+package ex5;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+
+
+	@Test
+	public void loginviaEmail(){
+		System.out.println("login via email");
+	}
+
+	@Test
+	public void loginViaFacebook(){
+		System.out.println("login via Facebook");
+	}
+
+	@Test
+	public void loginviatwitter(){
+		System.out.println("login via twitter");
+	}
+}
---
+++ Selenium/src/ex5/SignoutTest.java
@@ -0,0 +1,22 @@
+package ex5;
+
+import org.testng.annotations.Test;
+
+public class SignoutTest {
+
+	
+	@Test
+	public void signUpViaEmail(){
+		System.out.println("Signup via email");
+	}
+	
+	@Test
+	public void signUpViaFacebook(){
+		System.out.println("Signup via Facebook");
+	}
+	
+	@Test
+	public void signUpViaETwitter(){
+		System.out.println("Signup via Twitter");
+	}
+}
