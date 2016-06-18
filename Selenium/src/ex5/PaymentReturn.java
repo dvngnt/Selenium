82696ea099299a88c566fec2c79cd66231126e91
dvngnt <dgantt2@students.towson.edu>
2016-06-17 20:19:28 -0400

    Lecture 61 - Multiple Batch Files

---
+++ Selenium/src/ex5/PaymentReturn.java
@@ -0,0 +1,15 @@
+package ex5;
+
+import org.testng.annotations.Test;
+
+public class PaymentReturn {
+
+	
+	
+	@Test
+	public void paymentReturnByBank(){
+		System.out.println("return by bank");
+	}
+	
+	
+}
---
+++ Selenium/src/ex5/PaymentTest.java
@@ -0,0 +1,19 @@
+package ex5;
+
+import org.testng.annotations.Test;
+
+public class PaymentTest {
+
+	
+	
+	@Test
+	public void PaymentinDollar(){
+		System.out.println("Payment in Dollars");
+	}
+	
+	@Test
+	public void PaymentinRupee(){
+		System.out.println("Payment in Rupees");
+		
+	}
+}
