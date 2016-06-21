68fd170a38ad3c529509118d982a4ef98d074085
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-21 17:16:27 -0400

    Lecture 103 - Properties File

---
+++ Selenium/src/ex10/Property.java
@@ -0,0 +1,25 @@
+package ex10;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+public class Property {
+	
+	Properties property;
+	FileInputStream fs;
+	@Test
+	public void testProperty() throws IOException{
+
+		fs = new FileInputStream(System.getProperty("user.dir") + "\\src\\ex10\\config.properties");
+		property = new Properties();
+		property.load(fs);
+		System.out.println(property.getProperty("name"));
+
+	}
+
+}
