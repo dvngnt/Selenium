691933cd0426edb75b1b7169dcb317c576c2e6d9
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-21 20:08:27 -0400

    parallel browsers

--- Selenium/src/ex2/CssSelectors.java
+++ Selenium/src/ex2/CssSelectors.java
@@ -1,23 +1,85 @@
 package ex2;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
 
 
-public class CssSelectors {
+public class CssSelectors implements Runnable{
 
 	public static void main(String[] args){
-		
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://rediff.com");
-		driver.manage().window().maximize();
-		driver.findElement(By.linkText("Sign in")).click();
-		//tagName[attributename='attributeValue']
-    	//driver.findElement(By.cssSelector("input[id='login1']")).sendKeys("sally"); one property
-		//driver.findElement(By.cssSelector("input[id='login1'][name='login']")).sendKeys("two properties");
-		driver.findElement(By.cssSelector("#login1")).sendKeys("shortcut");
-		driver.findElement(By.cssSelector("#password")).sendKeys("sally");
+		//ChromeDriverManager.getInstance().setup();
+		//MarionetteDriverManager.getInstance().setup();
+		InternetExplorerDriverManager.getInstance().setup();
+	//	ChromeDriver driver1 = new ChromeDriver();
+		InternetExplorerDriver driver = new InternetExplorerDriver();
 
+		test(driver);
+	//	test(driver1);
+		
+	}
+	
+	public static void test (WebDriver driver){
+		driver.get("https://www.tripadvisor.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		
+		try {
+			driver.findElement(By.id("searchbox")).sendKeys("tokyo");
+			driver.findElement(By.tagName("body")).click();
+			driver.findElement(By.id("date_picker_in_1")).click();
+			 boolean found = false;
+			 String want = "October 2016";
+			 while(found == false){
+				 Thread.sleep(300);
+				 List<WebElement> list1 = driver.findElements(By.className("caption"));
+				 
+
+				 if(list1.get(0).getText().equals(want)){
+					 found = true;
+				 }
+				 else{
+					 
+					 driver.findElement(By.cssSelector(".next.sprite-date_picker-btnRightIdle")).click();;
+					 
+				 }
+			 }
+			 if(found == true){
+				 driver.findElement(By.cssSelector(".day.day_3")).click();
+				 driver.findElement(By.id("date_picker_out_1")).click();
+				 found = false;
+				 driver.findElement(By.cssSelector(".day.day_10")).click();
+				 driver.findElement(By.id("SUBMIT_HOTEL")).click();
+
+				
+			
+			 }
+		}
+			
+		
+		
+		
+		catch (Exception e) {
+			System.out.println("it isn't working");
+		}
+	}
+
+	public void run() {
+		InternetExplorerDriver driver = new InternetExplorerDriver();
+		test(driver);
+		
 	}
 }
