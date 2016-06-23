9cff570618c2aa0f89b3a1a583d0232f102b128c
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-23 19:30:32 -0400

    Selenium Grid bat files

---
+++ Selenium/src/ex13/start_hub.bat
@@ -0,0 +1,4 @@
+c:
+cd C:\Users\dgantt2\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+java -jar selenium-server-standalone-2.53.0.jar -role hub http://localhost:4444/grid/register -hubConfig C:\Users\dgantt2\git\Selenium\src\ex13\grid_hub.json
+cmd /k
