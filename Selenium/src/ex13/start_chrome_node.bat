7fea6a341ed22a9c31b40c01f988e223e3205465
dgantt2 <dgantt2@t1700-27.cattlab.umd.edu>
2016-06-24 11:53:17 -0400

    Removed hard coded paths

--- Selenium/src/ex13/start_chrome_node.bat
+++ Selenium/src/ex13/start_chrome_node.bat
@@ -1,3 +1,3 @@
 c:
-cd C:\Users\dgantt2\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
-java -jar selenium-server-standalone-2.53.0.jar -role node -nodeConfig C:\Users\dgantt2\git\Selenium\src\ex13\chrome_node.json
+cd %userprofile%\.m2\repository\org\seleniumhq\selenium\selenium-server-standalone\2.53.0
+java -jar selenium-server-standalone-2.53.0.jar -role node -nodeConfig %userprofile%\git\Selenium\src\ex13\chrome_node.json -Dwebdriver.chrome.driver=%userprofile%\.m2\repository\webdriver\chromedriver\win32\2.22\chromedriver.exe
